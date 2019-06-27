9c8146c397e183d9a999de8474913f901ccc472e
Izreel James <izreeljames@gmail.com>
2019-06-27 15:09:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var name = "Joe"
-var height = "74"
 
+var name = "Susan"
+var height = "70"
+
+ if (name === 'Susan'){
+   console.log('Yes, you are correct!')
+ } else {
+   conssole.log('Expected the name to be Susan')
+ }
+ 
+ 
 
 // Don't worry about this
 module.exports = { name, height
